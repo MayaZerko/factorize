510abd948d8e2fcd300c7d8416ad4afe9fb80727
isinka <altarika@icloud.com>
2023-03-08 15:01:40 +0200

    Add Multiprocessing to factorize function

--- multiprocesses.py
+++ multiprocesses.py
@@ -1,25 +1,29 @@
 from time import time
+from multiprocessing import cpu_count, Process, Pool
+from typing import List
 
 
-def factorize(*number):
-    result = []
-    for num in number:
-        result.append(
-            [i for i in range(1, num + 1) if num % i == 0]
-        )
-    print(result)
-    return result
+def counter(number: int) -> List[int]:
+    return [i for i in range(1, number + 1) if number % i == 0]
+
+
+def factorize_multiproces(*number):
+    with Pool(processes=cpu_count()- 2) as pool:
+        result = pool.map(counter, number)
+        print(result)
+        return result
 
 
 def main():
     start_time = time()
-    a, b, c, d = factorize(128, 255, 99999, 10651060)
+    a, b, c, d = factorize_multiproces(128, 255, 99999, 10651060)
     print(time() - start_time)
     assert a == [1, 2, 4, 8, 16, 32, 64, 128]
     assert b == [1, 3, 5, 15, 17, 51, 85, 255]
     assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
     assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580,
                  2130212, 2662765, 5325530, 10651060]
+    print(cpu_count())
 
 
 if __name__ == "__main__":
